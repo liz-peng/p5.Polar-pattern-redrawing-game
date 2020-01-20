f511604e42310e6b0d913eda9cb6ece570c50c49
lizpeng.tw <lizpeng.tw@gmail.com>
2020-01-19 23:10:06 -0500

    example 3

--- sketch.js
+++ sketch.js
@@ -33,3 +33,22 @@ let sketch2 = function(p) {
   };
 };
 let example2 = new p5(sketch2, 'sketch2');
+
+// example 3
+let sketch3 = function(p) { 
+  p.setup = function() {
+    let w = document.getElementById('sketch3').clientWidth;
+    let h = document.getElementById('sketch3').clientHeight;
+    p.createCanvas(w, h);
+    p.noFill();
+    p.stroke('#ccc');
+    p.strokeWeight(0.5); 
+    polar.setCenter(w/2, h/2);
+  }; 
+
+  p.draw = function() {
+    p.polarEllipse(0, 50, -50);
+    p.polarTriangles(2, 100, 0);
+  };
+};
+let example3 = new p5(sketch3, 'sketch3');

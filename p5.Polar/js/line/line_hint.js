764b6f1116708c5ac8408e15a5640b3217c2c87a
lizpeng.tw <lizpeng.tw@gmail.com>
2020-03-08 10:54:36 -0500

    [Line] Update measurement

--- p5.Polar/js/line/line_hint.js
+++ p5.Polar/js/line/line_hint.js
@@ -12,9 +12,9 @@ let m1 = function(m) {
     // text starts from here
     m.textSize(11);
     m.fill(204, 204, 204, 150);
-    m.text('Lv 1.1', -w/3-50, -h/3-30);
-    m.text('Radius 150', -w/3-50, -h/3-15);
-    m.text('0°', -3, -154);
+    m.text('# Lv 1.1', -w/3-50, -h/3-30);
+    m.text('Radius 120', -w/3-50, -h/3-15);
+    m.text('0°', -3, -120);
   }; 
 };
 let hint1 = new p5(m1, 'm1');
@@ -33,10 +33,10 @@ let m2 = function(m) {
     // text starts from here
     m.textSize(11);
     m.fill(204, 204, 204, 150);
-    m.text('Lv 1.2', -w/3-50, -h/3-30);
-    m.text('Radius 150', -w/3-50, -h/3-15);
-    m.text('0°', -3, -154);
-    m.text('90°', 162, 8);
+    m.text('# Lv 1.2', -w/3-50, -h/3-30);
+    m.text('Radius 120', -w/3-50, -h/3-15);
+    m.text('0°', -3, -120);
+    m.text('90°', 132, 12);
   }; 
 };
 let hint2 = new p5(m2, 'm2');
@@ -54,11 +54,11 @@ let m3 = function(m) {
     // text starts from here
     m.textSize(11);
     m.fill(204, 204, 204, 150);
-    m.text('Lv 1.3', -w/3-50, -h/3-30);
-    m.text('Radius 150', -w/3-50, -h/3-15);
+    m.text('# Lv 1.3', -w/3-50, -h/3-30);
+    m.text('Radius 120', -w/3-50, -h/3-15);
     m.text('Right Offset 50', -w/3-50, -h/3);
-    m.text('0°', -3, -154);
-    m.text('90°', -128, 10);
+    m.text('0°', -3, -120);
+    m.text('90°', 182, 12);
   }; 
 };
 let hint3 = new p5(m3, 'm3');
@@ -76,14 +76,14 @@ let m4 = function(m) {
     // text starts from here
     m.textSize(11);
     m.fill(204, 204, 204, 150);
-    m.text('Lv 1.4', -w/3-50, -h/3-30);
-    m.text('Radius 150', -w/3-50, -h/3-15);
-    m.text('0°', -3, -154);
-    m.text('30°', 76, -134);
-    m.text('60°', 138, -74);
-    m.text('90°', 162, 10);
-    m.text('120°', 140, 90);
-    m.text('150°', 78, 154);
+    m.text('# Lv 1.4', -w/3-50, -h/3-30);
+    m.text('Radius 120', -w/3-50, -h/3-15);
+    m.text('0°', -3, -120);
+    m.text('30°', 66, -102);
+    m.text('60°', 112, -56);
+    m.text('90°', 132, 12);
+    m.text('120°', 115, 80);
+    m.text('150°', 60, 134);
   }; 
 };
 let hint4 = new p5(m4, 'm4');

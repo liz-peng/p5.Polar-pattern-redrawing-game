520f76073e7757f977df70a3cf4cc887cbc65b7b
lizpeng.tw <lizpeng.tw@gmail.com>
2020-03-09 15:26:50 -0500

    [Line] update user drawing function

--- p5.Polar/js/line/line.js
+++ p5.Polar/js/line/line.js
@@ -1,15 +1,17 @@
 // 1.1
 const lv1_1Pattern = [
-	's.polarLine(0,150)'
+	's.polarLine(0,120)'
 ];
 
 const lv1_1 = (userInput, cm) => {
 	userInput = userInput.map(substr =>
+		substr.replace('(180,120', '(0,120')
+	).map(substr =>
 		substr.replace(',0)', ')')
 	);
 	
-	if(document.getElementById('defaultCanvas4')) {
-    document.getElementById('defaultCanvas4').remove();
+	if(document.getElementById('defaultCanvas8')) {
+    document.getElementById('defaultCanvas8').remove();
     document.getElementById('msg').innerText = '';
   } 
 
@@ -23,7 +25,7 @@ const lv1_1 = (userInput, cm) => {
       let h = document.getElementById('sketch1').clientHeight;
       s.createCanvas(w, h);
       s.noFill();
-      polar.setCenter(w/2, h/2);
+      polar.setCenter(w/2, h/2+10);
       if(!lv1_1Ans) {
       	s.stroke('#ccc');
       	s.strokeWeight(1); 
@@ -33,20 +35,18 @@ const lv1_1 = (userInput, cm) => {
 	    else {
       	s.strokeWeight(1); 
       	s.stroke('#cef19d');
-	      s.polarLine(0, 150, 0);
+	      s.polarLine(0, 120, 0);
 	      example1.drawingContext.lineWidth = 1;
-	      document.getElementById('msg').innerText = 'Pass!';
+	      document.getElementById('msg').innerText = 'Pass! (*´▽`*)';
 	      setTimeout(() => { 
-		      let lv1_2Val = '<h4>Level-1.2 Line</h4>'
+		      let lv1_2Val = '<h4>Level 1.2 Line</h4>'
 	          + '<p><hint><b>Mission</b></hint> ' 
-	          + 'Draw a vertical line, starts with <b>angle 0 and radius of 150</b>. '
-	          +	'And draw another horizontal line starts with <b>angle 90 and radius of 150</b> '
-	          + 'by calling polarLine() function twice</p> '
+	          + 'Draw two lines by calling polarLine() function twice.</p> '
 	          + '<p><hint><b>Hint</b></hint> <func><b>polarLine()</b></func> '
 	          + 'is the function to draw a single line. '
 	          + 'Three parameters can be passed to the single drawing funciton by the following order: '
 	          + '<b>angle</b>, <b>radius</b>, and <b>distance</b> (distance is an optional parameter) '
-	          + 'For example, polarLine(30, 50, 0) draws a line starts with 30 degree of angle and radius of 50</p>'
+	          + 'For example, polarLine(30, 50, 10) draws a line starts with 30 degree of angle, radius of 50, and 10 distance from center point.</p>'
 	          + '<p>* Type your answer in the box below and execute the code with <key><b>Shift + Enter</b></key> keys</p>';
 		      document.getElementById('intro').innerHTML = lv1_2Val;
 		      document.getElementById('msg').innerText = '';
@@ -61,17 +61,21 @@ const lv1_1 = (userInput, cm) => {
 
 // 1.2
 const lv1_2Pattern = [
-	's.polarLine(0,150)',
-  's.polarLine(90,150)'
+	's.polarLine(0,120)',
+  's.polarLine(90,120)'
 ];
 
 const lv1_2 = (userInput, cm) => {
 	userInput = userInput.map(substr =>
+		substr.replace('(180,120', '(0,120')
+	).map(substr =>
+		substr.replace('(270,120', '(90,120')
+	).map(substr =>
 		substr.replace(',0)', ')')
 	);
 	
-	if(document.getElementById('defaultCanvas5')) {
-    document.getElementById('defaultCanvas5').remove();
+	if(document.getElementById('defaultCanvas9')) {
+    document.getElementById('defaultCanvas9').remove();
     document.getElementById('msg').innerText = '';
   } 
 
@@ -89,7 +93,7 @@ const lv1_2 = (userInput, cm) => {
       let h = document.getElementById('sketch2').clientHeight;
       s.createCanvas(w, h);
       s.noFill();
-      polar.setCenter(w/2, h/2);
+      polar.setCenter(w/2, h/2+10);
       if(!lv1_2Ans) {
       	s.stroke('#ccc');
       	s.strokeWeight(1); 
@@ -99,24 +103,20 @@ const lv1_2 = (userInput, cm) => {
 	    else {
       	s.strokeWeight(1); 
       	s.stroke('#cef19d');
-	      s.polarLine(0, 150, 0);
+	      s.polarLine(0, 120, 0);
 	      s.stroke('#77bd98');
-    		s.polarLine(90, 150, 0);
-	      example2.drawingContext.lineWidth = 1;
-	      document.getElementById('msg').innerText = 'Pass!';
+    		s.polarLine(90, 120, 0);
+	      example2.drawingContext.lineWidth = 1; 
+	      document.getElementById('msg').innerText = 'Pass! (*´▽`*)';
 	      setTimeout(() => { 
-		      let lv1_3Val = '<h4>Level-1.3 Line</h4>'
+		      let lv1_3Val = '<h4>Level 1.3 Line</h4>'
 		      	+ '<p><hint><b>Mission</b></hint> '
-		      	+ 'Draw the same set of lines like previous level, a vertical line starts with '
-		      	+ '<b>angle 0 and radius of 150</b>. '
-		      	+ 'And another horizontal line starts with <b>angle 90 and radius of 150</b>, '
-		      	+ 'then give the horizontal line a <b>distance of 50</b>. '
-		      	+ 'Don\'t forget to call polarLine() function twice</p> '
+	          + 'Draw two lines by calling polarLine() function twice. Set distance value for the horizontal line.</p> '
 	          + '<p><hint><b>Hint</b></hint> <func><b>polarLine()</b></func> '
 	          + 'is the function to draw a single line. '
 	          + 'Three parameters can be passed to the single drawing funciton by the following order: '
 	          + '<b>angle</b>, <b>radius</b>, and <b>distance</b> (distance is an optional parameter) '
-	          + 'For example, polarLine(30, 50, 0) draws a line starts with 30 degree of angle and radius of 50</p>'
+	          + 'For example, polarLine(30, 50, 10) draws a line starts with 30 degree of angle, radius of 50, and 10 distance from center point.</p>'
 	          + '<p>* Type your answer in the box below and execute the code with <key><b>Shift + Enter</b></key> keys</p>';
 		      document.getElementById('intro').innerHTML = lv1_3Val;
 		      document.getElementById('msg').innerText = '';
@@ -131,17 +131,21 @@ const lv1_2 = (userInput, cm) => {
 
 // 1.3
 const lv1_3Pattern = [
-	's.polarLine(0,150)',
-  's.polarLine(90,150,50)'
+	's.polarLine(0,120)',
+  's.polarLine(90,120,50)'
 ];
 
 const lv1_3 = (userInput, cm) => {
 	userInput = userInput.map(substr =>
+		substr.replace('(180,120', '(0,120')
+	).map(substr =>
+		substr.replace('(270,120', '(90,120')
+	).map(substr =>
 		substr.replace(',0)', ')')
 	);
 
-	if(document.getElementById('defaultCanvas6')) {
-    document.getElementById('defaultCanvas6').remove();
+	if(document.getElementById('defaultCanvas10')) {
+    document.getElementById('defaultCanvas10').remove();
     document.getElementById('msg').innerText = '';
   } 
 
@@ -159,7 +163,7 @@ const lv1_3 = (userInput, cm) => {
       let h = document.getElementById('sketch3').clientHeight;
       s.createCanvas(w, h);
       s.noFill();
-      polar.setCenter(w/2, h/2);
+      polar.setCenter(w/2, h/2+10);
       if(!lv1_3Ans) {
       	s.stroke('#ccc');
       	s.strokeWeight(1); 
@@ -169,22 +173,20 @@ const lv1_3 = (userInput, cm) => {
 	    else {
       	s.strokeWeight(1); 
 	      s.stroke('#cef19d');
-	      s.polarLine(0, 150, 0);
+	      s.polarLine(0, 120, 0);
 	      s.stroke('#77bd98');
-    		s.polarLine(90, 150, 50);
+    		s.polarLine(90, 120, 50);
 	      example3.drawingContext.lineWidth = 1;
-	      document.getElementById('msg').innerText = 'Pass!';
+	      document.getElementById('msg').innerText = 'Pass! (*´▽`*)';
 	      setTimeout(() => { 
-		      let lv1_4Val = '<h4>Level-1.4 Line</h4>'
+		      let lv1_4Val = '<h4>Level 1.4 Line</h4>'
 		      	+ '<p><hint><b>Mission</b></hint> '
-		      	+ 'Draw six lines with <b>radius of 150</b> '
-		      	+ 'and with six different angles <b>0, 30, 60, 90, 120 and 150</b>. '
-		      	+ 'Call polarLine() function six times. Each of line function should have different angle</p>'
+		      	+ 'Draw six lines by calling polarLine() function six times with different angles.</p> '
 	          + '<p><hint><b>Hint</b></hint> <func><b>polarLine()</b></func> '
 	          + 'is the function to draw a single line. '
 	          + 'Three parameters can be passed to the single drawing funciton by the following order: '
 	          + '<b>angle</b>, <b>radius</b>, and <b>distance</b> (distance is an optional parameter) '
-	          + 'For example, polarLine(30, 50, 0) draws a line starts with 30 degree of angle and radius of 50</p>'
+	          + 'For example, polarLine(30, 50, 10) draws a line starts with 30 degree of angle, radius of 50, and 10 distance from center point.</p>'
 	          + '<p>* Type your answer in the box below and execute the code with <key><b>Shift + Enter</b></key> keys</p>';
 		      document.getElementById('intro').innerHTML = lv1_4Val;
 		      document.getElementById('msg').innerText = '';
@@ -199,21 +201,33 @@ const lv1_3 = (userInput, cm) => {
 
 // // 1.4
 const lv1_4Pattern = [
-	's.polarLine(0,150)',
- 	's.polarLine(30,150)',
-  's.polarLine(60,150)',
-  's.polarLine(90,150)',
-  's.polarLine(120,150)',
-  's.polarLine(150,150)'
+	's.polarLine(0,120)',
+ 	's.polarLine(30,120)',
+  's.polarLine(60,120)',
+  's.polarLine(90,120)',
+  's.polarLine(120,120)',
+  's.polarLine(150,120)'
 ];
 
 const lv1_4 = (userInput, cm) => {
 	userInput = userInput.map(substr =>
+		substr.replace('(180,120', '(0,120')
+	).map(substr =>
+		substr.replace('(210,120', '(30,120')
+	).map(substr =>
+		substr.replace('(240,120', '(60,120')
+	).map(substr =>
+		substr.replace('(270,120', '(90,120')
+	).map(substr =>
+		substr.replace('(300,120', '(120,120')
+	).map(substr =>
+		substr.replace('(330,120', '(150,120')
+	).map(substr =>
 		substr.replace(',0)', ')')
 	);
 
-	if(document.getElementById('defaultCanvas7')) {
-    document.getElementById('defaultCanvas7').remove();
+	if(document.getElementById('defaultCanvas11')) {
+    document.getElementById('defaultCanvas11').remove();
     document.getElementById('msg').innerText = '';
   } 
 
@@ -231,7 +245,7 @@ const lv1_4 = (userInput, cm) => {
       let h = document.getElementById('sketch4').clientHeight;
       s.createCanvas(w, h);
       s.noFill();
-      polar.setCenter(w/2, h/2);
+      polar.setCenter(w/2, h/2+10);
       if(!lv1_4Ans) {
       	s.stroke('#ccc');
       	s.strokeWeight(1); 
@@ -241,23 +255,25 @@ const lv1_4 = (userInput, cm) => {
 	    else {
       	s.strokeWeight(1); 
 	      s.stroke('#cef19d');
-	      s.polarLine(0, 150, 0);
+	      s.polarLine(0, 120, 0);
 	      s.stroke('#a6dba7');
-				s.polarLine(30, 150, 0);
+				s.polarLine(30, 120, 0);
 				s.stroke('#77bd98');
-				s.polarLine(60, 150, 0);
+				s.polarLine(60, 120, 0);
 				s.stroke('#f9c6b3');
-				s.polarLine(90, 150, 0);
+				s.polarLine(90, 120, 0);
 				s.stroke('#faaa8f');
-				s.polarLine(120, 150, 0);
+				s.polarLine(120, 120, 0);
 				s.stroke('#e29686');
-				s.polarLine(150, 150, 0);
+				s.polarLine(150, 120, 0);
 	      example4.drawingContext.lineWidth = 1;
-	      document.getElementById('msg').innerText = 'Pass!';
+	      document.getElementById('msg').innerText = 'Pass! (*´▽`*)';
 	      setTimeout(() => { 
 		      let nextLevel = '<h3>Congratulations! (*´▽`*)</h3>'
-		      + '<p>You just learned how to draw lines with single drawing function. Click the button below and move to the next level</p>'
-		      + '<button onclick="window.location.href = \'../triangle/index.html\'">Next Level - Triangle</button>';
+		      + '<p>You just learned how to draw lines with p5.Polar single drawing function. ' 
+		      + '<b>Multiple drawing function will be introduced in the future</b>. '
+		      + 'Click the button below and move to the next level.</p>'
+		      + '<button onclick="window.location.href = \'../level/triangle.html\'">Next Level - Triangle</button>';
 	        document.getElementById('intro').innerHTML = nextLevel;
 		      document.getElementById('msg').innerText = '';
 		    }, 1500);
